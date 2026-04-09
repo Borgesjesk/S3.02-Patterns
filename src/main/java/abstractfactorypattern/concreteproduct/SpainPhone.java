7c2065bc725820b3022f028d63ee17176585ffb6
BORGES <jessborgesb@gmail.com>
2026-04-09 17:22:31 +0200

    feat: add SpainPhone concrete product

--- src/main/java/abstractfactorypattern/concreteproduct/SpainPhone.java
+++ src/main/java/abstractfactorypattern/concreteproduct/SpainPhone.java
@@ -1,4 +1,17 @@
 package abstractfactorypattern.concreteproduct;
 
-public class SpainPhone {
+import abstractfactorypattern.abstractproduct.Phone;
+
+public class SpainPhone implements Phone {
+
+    String number;
+
+    public SpainPhone(String number) {
+        this.number = number;
+    }
+
+    @Override
+    public String getPhoneNumber() {
+        return ("Phone: +34 " + number);
+    }
 }
