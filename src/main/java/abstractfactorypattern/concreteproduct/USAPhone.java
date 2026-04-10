ad15c4a49bcbeb290dd5876dab731302bd11fb29
BORGES <jessborgesb@gmail.com>
2026-04-10 10:05:15 +0200

    feat: add USAPhone concrete product

--- src/main/java/abstractfactorypattern/concreteproduct/USAPhone.java
+++ src/main/java/abstractfactorypattern/concreteproduct/USAPhone.java
@@ -1,4 +1,17 @@
 package abstractfactorypattern.concreteproduct;
 
-public class USAPhone {
+import abstractfactorypattern.abstractproduct.Phone;
+
+public class USAPhone implements Phone {
+
+    String number;
+
+    public USAPhone(String number) {
+        this.number = number;
+    }
+
+    @Override
+    public String getPhoneNumber() {
+        return ("Phone: +1 " + number);
+    }
 }
