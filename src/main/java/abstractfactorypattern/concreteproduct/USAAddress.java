1ad1643293835cabf625b44a9d4026b0ec429105
BORGES <jessborgesb@gmail.com>
2026-04-10 09:59:04 +0200

    feat: add USAAddress concrete product

--- src/main/java/abstractfactorypattern/concreteproduct/USAAddress.java
+++ src/main/java/abstractfactorypattern/concreteproduct/USAAddress.java
@@ -1,4 +1,21 @@
 package abstractfactorypattern.concreteproduct;
 
-public class USAAddress {
+import abstractfactorypattern.abstractproduct.Address;
+
+public class USAAddress implements Address {
+
+    String street;
+    String city;
+    String stateZip;
+
+    public USAAddress(String street, String city, String stateZip) {
+        this.street = street;
+        this.city = city;
+        this.stateZip = stateZip;
+    }
+
+    @Override
+    public String getAddress() {
+        return ("Address: " + street + ", " + city + ", " + stateZip);
+    }
 }
